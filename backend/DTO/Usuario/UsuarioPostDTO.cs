0aee6aae50f0a2b29cc6e2f582ab0b809efe0924
gustavogogola91 <gustavogogola3@gmail.com>
2025-06-25 15:40:42 -0300

    Adicionado UsuarioPostDTO

---
+++ backend/DTO/Usuario/UsuarioPostDTO.cs
@@ -0,0 +1,15 @@
+using backend.Model.Enum;
+
+namespace backend.DTO
+{
+    public class UsuarioPostDTO
+    {
+        public required string Nome { get; set; }
+        public required string Username { get; set; }
+        public required string Senha { get; set; }
+        public required string Email { get; set; }
+        public required bool Ativo { get; set; }
+        public required TipoUsuario Tipo { get; set; }
+        public required ICollection<GrupoSuporte> Grupos { get; set; }
+    }
+}
