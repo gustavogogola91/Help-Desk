d33dc38331bcd64f523476e4ba0865db0e784a57
gustavogogola91 <gustavogogola3@gmail.com>
2025-06-25 23:08:45 -0300

    Adicionado Validator para Chamado

---
+++ backend/Validators/ChamadoValidator.cs
@@ -0,0 +1,56 @@
+using backend.DTO;
+using backend.Interfaces;
+using FluentValidation;
+
+namespace backend.Validators
+{
+    public class ChamadoValidator : AbstractValidator<ChamadoPostDTO>
+    {
+        // TODO: Adicionar EquipamentoRepository para validação de existencia do equipamento
+
+        private readonly IEquipamentoRepository _equipamentoRepository;
+        private readonly ISetorRepository _setoreRepository;
+        private readonly IEstabelecimentoRepository _estabelecimentoRepository;
+
+        public ChamadoValidator(IEquipamentoRepository equipamentoRepository, ISetorRepository setorRepository, IEstabelecimentoRepository estabelecimentoRepository)
+        {
+            _equipamentoRepository = equipamentoRepository;
+            _setoreRepository = setorRepository;
+            _estabelecimentoRepository = estabelecimentoRepository;
+
+            RuleFor(c => c.NomeSolicitante)
+                .NotEmpty().WithMessage("{PropertyName}  é obrigatório")
+                .MaximumLength(60).WithMessage("{PropertyName} deve conter no máximo {MaxLength} caracteres");
+
+            RuleFor(c => c.Descricao)
+                .NotEmpty().WithMessage("{PropertyName}  é obrigatório");
+
+            RuleFor(c => c.EquipamentoId)
+                .GreaterThan(0).WithMessage("O ID do equipamento deve ser maior que zero")
+                .MustAsync(async (setorId, cancellation) => await _equipamentoRepository.ExisteAsync(setorId)).WithMessage("O equipamento com o ID especificado não foi encontrado.");
+
+            RuleFor(c => c.SetorSolicitanteId)
+                .GreaterThan(0).WithMessage("O ID do setor deve ser maior que zero")
+                .MustAsync(async (setorId, cancellation) => await _setoreRepository.ExisteAsync(setorId)).WithMessage("O Setor com o ID especificado para {PropertyName} não foi encontrado.");
+
+            RuleFor(c => c.SetorDestinoId)
+                .GreaterThan(0).WithMessage("O ID do setor deve ser maior que zero")
+                .MustAsync(async (setorId, cancellation) => await _setoreRepository.ExisteAsync(setorId)).WithMessage("O Setor com o ID especificado para {PropertyName} não foi encontrado.");
+
+            RuleFor(c => c.EstabelecimentoId)
+                .GreaterThan(0).WithMessage("O ID do equipamento deve ser maior que zero")
+                .MustAsync(async (estabelecimentoId, cancellation) => await _estabelecimentoRepository.ExisteAsync(estabelecimentoId)).WithMessage("O Estabelecimento com o ID especificado não foi encontrado.");
+
+            RuleFor(c => c.Ramal)
+                .NotEmpty().WithMessage("{PropertyName}  é obrigatório")
+                .MaximumLength(6).WithMessage("{PropertyName} deve conter no máximo {MaxLength} caracteres");
+
+            RuleFor(c => c.Computador)
+                .NotEmpty().WithMessage("{PropertyName}  é obrigatório");
+
+            RuleFor(c => c.Ip)
+                .NotEmpty().WithMessage("{PropertyName}  é obrigatório")
+                .MaximumLength(15).WithMessage("{PropertyName} deve conter no máximo {MaxLength} caracteres");
+        }
+    }
+}
