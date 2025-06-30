e5586dc8349f153f551449150e53a23bda2dab40
gustavogogola91 <gustavogogola3@gmail.com>
2025-06-30 17:46:52 -0300

    Pequena alteração para não retornar o id nulo

--- backend/Middleware/ExceptionMiddleware.cs
+++ backend/Middleware/ExceptionMiddleware.cs
@@ -31,7 +31,7 @@ namespace backend.Middleware
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.NotFound;
 
-                var response = new APIException(context.Response.StatusCode.ToString(), ex.Message, $"Id: {ex.Id}");
+                var response = new APIException(context.Response.StatusCode.ToString(), ex.Message, ex.Id == null ? $"Id: {ex.Id}" : "Não informado");
 
                 var options = new JsonSerializerOptions
                 {
