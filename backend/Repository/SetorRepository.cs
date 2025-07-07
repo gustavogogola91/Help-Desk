2be81fa122cd6160ce298cb26541cac63c92d688
gustavogogola91 <gustavogogola3@gmail.com>
2025-07-07 13:19:15 -0300

    Finalizado endpoint setor

--- backend/Repository/SetorRepository.cs
+++ backend/Repository/SetorRepository.cs
@@ -1,16 +1,45 @@
+using backend.Helpers;
 using backend.Interfaces;
+using backend.Model;
 using Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Repository
 {
-    public class SetorRepository(AppDbContext database) : ISetorRepository
+    public class SetorRepository(AppDbContext database, IPaginationHelper pagination) : ISetorRepository
     {
         private readonly AppDbContext _database = database;
+        private readonly IPaginationHelper _pagination = pagination;
 
         public async Task<bool> ExisteAsync(long id)
         {
             return await _database.tb_setor.AnyAsync(s => s.Id == id);
+        }
+
+        public async Task<List<Setor>> GetAllSetores()
+        {
+            var setores = await _database.tb_setor.Where(s => s.Ativo).ToListAsync();
+            return setores;
+        }
+
+        public async Task<PagedList<Setor>> GetAllSetoresPaged(int currentPage)
+        {
+            var query = _database.tb_setor.AsQueryable();
+            var pagedList = await _pagination.CreateAsync(query, currentPage, 10);
+
+            return pagedList;
+        }
+
+        public async Task<Setor> GetSetorById(long id)
+        {
+            var usuario = await _database.tb_setor.FirstOrDefaultAsync(s => s.Id == id);
+            return usuario!;
+        }
+
+        public async Task SalvarAlteracao(Setor setor)
+        {
+            _database.tb_setor.Update(setor);
+            await _database.SaveChangesAsync();
         }
     }
 }
